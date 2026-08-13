cd218b7dc94e1df17541b751ec8c1606d9d70207
Richard <richard.dev@icloud.com>
2026-08-12 23:09:13 -0400

    add xshop nextjs app

---
+++ xshop/app/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Link from "next/link";
+import Login from "./login/page";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Welcome to XShop</h1>
+
+      <button onClick={() => { router.push('/login') }}>LOGIN</button>
+      <br />
+      <Link href="/shop">Go to Shop</Link>
+    </div>
+  );
+}
